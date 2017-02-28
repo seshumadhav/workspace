cf9cc3bb9ae5096863432d263065898e5d92d77d
Seshumadhav Chaturvedula <seshu@indeed.com>
2017-03-01 08:53:04 +0900

    Successfully retrieving all dependencies from mvncentral. Probably ending in infinite loop

--- scraping-ruby/myspider.rb
+++ scraping-ruby/myspider.rb
@@ -27,7 +27,7 @@ def is_valid_node(url)
 end	
 
 def need_not_proceed(href, text) 
-	return href.to_s.empty? || (text.to_s.empty? || $NON_PROCEEDABLE_DIRS.include? (text))
+	return href.to_s.empty? || text.to_s.empty? || $NON_PROCEEDABLE_DIRS.include?(text)
 end
 
 
@@ -42,7 +42,7 @@ def append(url1, url2)
 end
 
 def is_relative_link(href)
-	href.start_with? ('/')
+	href.end_with? ('/')
 end
 
 root_node = {
@@ -60,11 +60,14 @@ def scrape(url)
 	node = $urls[url]
 
 	# If page already scraped, abort scraping that page and return
-	return if node[:processed]
+	if node[:processed]
+		puts "Scraping aborted for #{url}. REASON: Already scraped"
+	end	
 	
 	# If one of the terminal links like XML, JAR, SHA1, mark the node processed and return; No scraping required
 	if url_ends_with_jar_or_xml_sha1(url)
 		node[:processed] = true
+		puts "Scraping aborted for #{url}. REASON: Terminal Link encountered."
 		return
 	end	
 
@@ -74,24 +77,27 @@ def scrape(url)
 	pageobj = nil
 	begin
 		pageobj = get_page_content_as_nokogiri_obj(url)
-	rescue
-		puts "Exception while opening #{url}"
+	rescue => ex
+		puts "Scraping aborted for #{url}. REASON: Exception #{ex} while opening."
 		return
 	end	
 
 	# Extract all anchor tags. If not anchor tags, abort scraping and return.
 	atags = pageobj.css('a')
-	return if atags.to_a.empty?
+	if atags.to_a.empty?
+		puts "Scraping aborted for #{url}. REASON: No anchor tags found"
+		return 
+	end
 
 	# Process each anchor tag in the page
 	atags.each do |atag|
 		href = atag['href']
-		puts "\nhref: #{href}"
+		# puts "\nhref: #{href}"
 		text = atag.text.to_s.empty? ? "" : atag.text.chomp('/')
 
 		# Do not go deep if the text on link is 'Parent Directory' or if that is an empty link
 		if (need_not_proceed(href, text))
-			puts "Chasing: NO"
+			# puts "Chasing: NO"
 			next
 		end
 
@@ -107,9 +113,14 @@ def scrape(url)
 		# if the urls map already contains this node's direct link, skip initiating a call to that URL.
 		if !$urls.has_key?(direct_link_to_this_node)
 			$urls[direct_link_to_this_node] = child_node	
+			puts "\nChasing #{direct_link_to_this_node}"
 			scrape(direct_link_to_this_node)
 		end
 	end
 end
 
 scrape(root_url)
+puts "\n\nList of all libraries found: \n"
+$urls.each do |direct_url, url|
+	puts "#{url[:canonical_name]}"
+end
